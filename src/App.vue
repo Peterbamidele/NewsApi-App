ae9df66169b8c14957dac9bd9f548f246a660da2
peter aro <peterbamidele98@gmail.com>
2022-01-12 22:10:12 +0100

    latest

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,18 @@
     <h2 class="mb-8 text-4xl font-bold text-center capitalize">
       News Section : <span class="text-green-700">{{ section }}</span>
     </h2>
-    <NewsFilter v-model="section" />
+    <NewsFilter v-model="section"  :fetch="fetchNews"/>
     <NewsList :posts="posts" />
   </Layout>
 </template>
 
 <script>
-import Layout from "./components/Layout"
-import NewsFilter from "./components/NewsFilter"
+import Layout from "@/components/Layout"
+import NewsFilter from "@/components/NewsFilter"
 import NewsList from "@/components/NewsList";
+
+// import axios from "axios"
+// const api = import.meta.env.VITE_NYT_API_KEY
 
 import data from "./posts.json"
 export default {
@@ -23,8 +26,54 @@ export default {
   data(){
     return {
       section: "home",
-      posts: data.posts,
+       posts: data.posts,
+      // kiosk
+      //posts: [],
     }
   },
+//   methods :{
+//     extractImage(post){
+//       const defaultImg ={
+//         url:" http://placehold.it/210x140?text=N/A",
+//         caption: post.title,
+//       }
+//       if(!post.multimedia){
+//         return defaultImg
+//       }
+//     let imgObj = post.multimedia.find(
+//         media => media.format === "mediumThreeByTwo210"
+//     )
+//     return imgObj ? imgObj : defaultImg
+//   },
+//   async fetchNews() {
+//     try {
+//       const url = `https://api.nytimes.com/svc/topstories/v2/${this.section}.json?api-key=${api}`
+//       const response = await axios.get(url)
+//       const results = response.data.results
+//       this.posts = results.map(post => ({
+//         title: post.title,
+//         abstract: post.abstract,
+//         url: post.url,
+//         thumbnail: this.extractImage(post).url,
+//         caption: this.extractImage(post).caption,
+//         byline: post.byline,
+//         published_date: post.published_date,
+//       }))
+//     } catch (err) {
+//       if (err.response) {
+//         // client received an error response (5xx, 4xx)
+//         console.log("Server Error:", err)
+//       } else if (err.request) {
+//         // client never received a response, or request never left
+//         console.log("Network Error:", err)
+//       } else {
+//         console.log("Client Error:", err)
+//       }
+//     }
+//   },
+// },
+// mounted() {
+//   this.fetchNews()
+// },
 }
 </script>
--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,10 +1,23 @@
 <template>
-<div></div>
+  <div
+      class="grid grid-cols-1 gap-6 mt-4 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 justify-items-center"
+  >
+    <NewsCard v-for="(post, index) in posts" :key="index" :post="post" />
+  </div>
 </template>
 
 <script>
+import NewsCard from "@/components/NewsCard";
 export default {
-  name: "NewsList"
+  props:{
+    posts:{
+      type:Array,
+      required: true,
+    },
+  },
+  components:{
+    NewsCard,
+  },
 }
 </script>
 
